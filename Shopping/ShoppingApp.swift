2d67b0dea4df22c3274a0334c0a4a920a72b82b4
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-22 17:03:07 -0400

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Sai Lakshmi on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
